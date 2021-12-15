80c06c839288f9abb997bb48bf69653054d35916
yintellij <yj@laniakealab.io>
2021-12-15 16:03:01 +0800

    build

--- debug.sh
+++ debug.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env zsh
 nasm -o mbr.bin mbr.S
+nasm -o loader.bin loader.S
 dd if=mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
+dd if=loader.bin of=hd60M.img bs=512 count=1 seek=1 conv=notrunc
