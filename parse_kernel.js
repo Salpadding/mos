17162693411de0b29e31e9da7fee4dbcaed3dfe9
Salpadding <m6567fc@outlook.com>
2021-12-19 23:06:17 +0800

    add assertions

--- parse_kernel.js
+++ parse_kernel.js
@@ -2,6 +2,26 @@ const fs = require('fs')
 const path = require('path')
 const bin = fs.readFileSync(path.join(__dirname, 'kernel.bin'))
 
+// memory
+const mem = fs.readFileSync(path.join(__dirname, 'dump.bin'))
+
+
+// 1. assert kernel load success
+for(let i = 0; i < bin.length; i++) {
+    if (mem[i + 0x70000] != bin[i]) {
+        console.log('memory load failed')
+        process.exit(1)
+    }
+}
+
+
+// for(let i = 0x70000 + bin.length; i < mem.length; i++) {
+//     if (mem[i] !== 0) {
+//         console.log(`write overflow at 0x${i.toString(16)}`)
+//         process.exit(1)
+//     }
+// }
+
 // size of program header = 32 byte
 const phentsize = bin.readUInt16LE(42)
 // offset of program header, 0x34 52
@@ -9,6 +29,9 @@ const phoff = bin.readUInt32LE(28)
 // number of program header 
 const phnum = bin.readUInt16LE(44)
 
+
+
+// assert segment load success
 for(let i = 0; i < phnum; i++) {
     const off = phoff + i * phentsize
     const type = bin.readUInt32LE(off)
@@ -24,6 +47,14 @@ for(let i = 0; i < phnum; i++) {
         const pAddr = bin.readUInt32LE(off + 12)
         const fileSz = bin.readUInt32LE(off + 16)
         const memSz = bin.readUInt32LE(off + 20)
+
+
+        for(let j = 0; j < fileSz; j++) {
+            if(mem[vAddr + j] !== bin[segOff + j]) {
+                console.log(`segment load failed ${i}`)
+                process.exit(1)
+            }
+        }
 
         console.log ({
             type: type === 0x6474e551 ? 'PT_GNU_STACK' : type.toString(),
