a28e765357a892c7c579c7412b9612aa6876eaab
Salpadding <m6567fc@outlook.com>
2021-12-20 17:35:28 +0800

    print gdt in clang

--- kernel/main.c
+++ kernel/main.c
@@ -4,13 +4,23 @@
 #define VGA_COLS 80
 #define LOADER_DATA (0x800 + 8)
 
+// represent in binary
+#define REPR_B __attribute__((packed))
+
 typedef unsigned long u32_t;
 typedef unsigned short u16_t;
+typedef unsigned long long u64_t;
+
+struct gdt_ptr
+{
+    u16_t gdt_bound; // gdt_size - 1
+    u64_t *gdt_base
+} REPR_B;
 
 struct loader_data_st
 {
-    u32_t gdt_ptr_addr
-};
+    struct gdt_ptr* gdt_ptr_addr
+} REPR_B;
 
 // vga col  [0, 79]
 static u32_t vga_col = 0;
@@ -24,10 +34,21 @@ void put_hex(u32_t u);
 int main()
 {
     puts("hello world!\n");
-    struct loader_data_st * d = LOADER_DATA;
+    struct loader_data_st *d = LOADER_DATA;
 
+    puts("address of gdt_ptr:\n");
     put_hex(d->gdt_ptr_addr);
     put_char('\n');
+
+    puts("bound of gdt:\n");
+    put_hex(d->gdt_ptr_addr->gdt_bound);
+    put_char('\n');
+
+
+    puts("base of gdt:\n");
+    put_hex(d->gdt_ptr_addr->gdt_base);
+    put_char('\n');
+    
 
     int a = 0;
     int b = 0;
@@ -53,18 +74,18 @@ int cls()
 
 void put_hex(u32_t x)
 {
-    static const char* chars = "0123456789abcdef";
+    static const char *chars = "0123456789abcdef";
     if (x == 0)
     {
         put_char('0');
         return;
     }
 
-    #define BUF_SIZE 32
+#define BUF_SIZE 32
     char buf[BUF_SIZE];
     int i = 0;
 
-    while(x != 0)
+    while (x != 0)
     {
         u32_t y = x % 16;
         x = x / 16;
@@ -74,7 +95,7 @@ void put_hex(u32_t x)
 
     char buf2[BUF_SIZE];
 
-    for(int j = 0; j < i; j++) 
+    for (int j = 0; j < i; j++)
     {
         buf2[j] = buf[i - j - 1];
     }
