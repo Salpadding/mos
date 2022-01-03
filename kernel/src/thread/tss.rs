7a2740876179a5c7d976078ad3f1ad32fd132b13
zhuyingjie <m6567fc@outlook.com>
2022-01-03 22:03:11 +0800

    tss macro

--- kernel/src/thread/tss.rs
+++ kernel/src/thread/tss.rs
@@ -1,6 +1,6 @@
-use rlib::gdt::gdt;
 use crate::asm::SELECTOR_TSS;
 use crate::println;
+use rlib::gdt::gdt;
 
 pub const TSS_LEN: usize = 27;
 pub const TSS_SIZE: usize = TSS_LEN * 4;
@@ -9,50 +9,64 @@ pub const TSS_BOUND: usize = TSS_SIZE - 1;
 pub static mut TSS_DATA: [u32; TSS_LEN] = [0u32; TSS_LEN];
 
 macro_rules! m {
-    ($f: ident, $fm: ident) => {
-        fn $f(&self) -> u32;
-        fn $fm(&mut self) -> &mut u32;
-    };
-}
-
-macro_rules! im {
     ($f: ident, $fm: ident, $i: expr) => {
-        fn $f(&self) -> u32 { self[$i] }
-        fn $fm(&mut self) -> &mut u32 { &mut self[$i] }
+        fn $f(&self) -> u32 {
+            self.slice()[$i]
+        }
+        fn $fm(&mut self) -> &mut u32 {
+            &mut self.slice_mut()[$i]
+        }
     };
 }
 
 pub trait TSS {
-    m!(back_link, back_link_mut);
-    m!(esp0, esp0_mut);
-    m!(ss0, ss0_mut);
-    // esp1
-    // ss1
-    // esp2
-    // ss2
-    // cr3
-    // eip
-    // e_flags
-    // eax
-    // ecx
-    // edx
-    // ebx
-
+    fn slice(&self) -> &[u32];
+    fn slice_mut(&mut self) -> &mut [u32];
+    m!(back_link, back_link_mut, 0);
+    m!(esp0, esp0_mut, 1);
+    m!(ss0, ss0_mut, 2);
+    m!(esp1, esp1_mut, 3);
+    m!(ss1, ss1_mut, 4);
+    m!(esp2, esp2_mut, 5);
+    m!(ss2, ss2_mut, 6);
+    m!(cr3, cr3_mut, 7);
+    m!(eip, eip_mut, 8);
+    m!(e_flags, e_flags_mut, 9);
+    m!(eax, eax_mut, 10);
+    m!(ecx, ecx_mut, 11);
+    m!(edx, edx_mut, 12);
+    m!(ebx, ebx_mut, 13);
+    m!(esp, esp_mut, 14);
+    m!(ebp, ebp_mut, 15);
+    m!(esi, esi_mut, 16);
+    m!(edi, edi_mut, 17);
+    m!(es, es_mut, 18);
+    m!(cs, cs_mut, 19);
+    m!(ss, ss_mut, 20);
+    m!(ds, ds_mut, 21);
+    m!(fs, fs_mut, 22);
+    m!(gs, gs_mut, 23);
+    m!(ldt, ldt_mut, 24);
+    m!(trace, trace_mut, 25);
+    m!(io_base, io_base_mut, 26);
 }
 
 impl TSS for [u32] {
-    im!(back_link, back_link_mut, 0);
-    im!(esp0, esp0_mut, 1);
-    im!(ss0, ss0_mut, 2);
+    fn slice(&self) -> &[u32] {
+        &self
+    }
+
+    fn slice_mut(&mut self) -> &mut [u32] {
+        self
+    }
 }
 
 const GDT_OFF: usize = 0x800 + 8;
 const GDT_LEN: usize = 8;
 
 pub fn init() {
-
     // 0, 1, 2 is predefined
-   let gdt = gdt(GDT_OFF, GDT_LEN);
+    let gdt = gdt(GDT_OFF, GDT_LEN);
 
     // 3 is user code
     gdt[3] = rlib::gdt::user_code();
@@ -60,14 +74,16 @@ pub fn init() {
     gdt[4] = rlib::gdt::user_data();
 
     let mut bd = rlib::gdt::GdtBuilder::default();
-    gdt[5] = bd.present(true).base(unsafe { TSS_DATA.as_ptr() as usize as u32 })
+    gdt[5] = bd
+        .present(true)
+        .base(unsafe { TSS_DATA.as_ptr() as usize as u32 })
         .limit(TSS_BOUND as u32)
         .access(true)
         .privilege(0)
         .system(true)
         .lim_4k(true)
-        .executable(true).build();
-
+        .executable(true)
+        .build();
 
     println!("gdt ptr = {}", crate::asm::gdt());
     println!("{:?}", gdt);
