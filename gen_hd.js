7c94888ba9d32a8b2328375e73611e1ca512a5a3
zhuyingjie <m6567fc@outlook.com>
2022-02-17 23:29:37 +0800

    fix

--- gen_hd.js
+++ gen_hd.js
@@ -1,6 +1,7 @@
+#!node
 const fs = require('fs')
 const path = require('path')
-const dst = path.join(__dirname, 'build/disk-fs.dmg')
+const dst = path.join(__dirname, 'build/disk-fs.img')
 const src = path.join(__dirname, 'partition_table');
 
 const bin = Buffer.alloc(67092480)
